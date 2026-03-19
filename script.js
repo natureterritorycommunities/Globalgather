c29c4a07e4dd76a644da56a0426cf3e80be94187
natureterritorycommunities <nature.territory.communities@gmail.com>
2026-03-19 05:24:03 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
+
 let currentLang='en';
 
 function toggleLang(){
   currentLang=currentLang==='en'?'es':'en';
-
   document.querySelectorAll('[data-en]').forEach(el=>{
     el.innerText=el.getAttribute('data-'+currentLang);
   });
 }
 
-window.onload=()=>toggleLang();
+// CAROUSEL AUTO
+let slides=document.querySelectorAll('.slide');
+let index=0;
+
+function showSlides(){
+  slides.forEach(s=>s.classList.remove('active'));
+  index=(index+1)%slides.length;
+  slides[index].classList.add('active');
+}
+
+setInterval(showSlides,4000);
+
+window.onload=()=>{
+  toggleLang();
+  slides[0].classList.add('active');
+};
